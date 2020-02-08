db8ee8b14d099d39ea8d4c465ed4e9735d853594
NullVoxPopuli <LPSego3+dev@gmail.com>
2020-02-08 18:09:08 -0500

    03 - add the ability to mark todos as complete so that Clear Complete in the footer can do something

---
+++ steps/03-adding-interactivity/todomvc/app/components/todo.js
@@ -0,0 +1,7 @@
+import Component from '@glimmer/component';
+import { inject as service } from '@ember/service';
+
+export default class TodoComponent extends Component {
+  @service('todo-data') todos;
+
+}
--- steps/03-adding-interactivity/todomvc/app/services/todo-data.js
+++ steps/03-adding-interactivity/todomvc/app/services/todo-data.js
@@ -35,4 +35,9 @@ export default class TodoDataService extends Service {
   clearCompleted() {
     this.todos = this.incomplete;
   }
+
+  @action
+  toggleCompletion(todo) {
+    todo.isCompleted = !todo.isCompleted;
+  }
 }
